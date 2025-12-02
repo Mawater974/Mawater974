3cc5c90ca7cf27097b7d5d0cd3c41716f79647bc
Mawater974 <mawater974@gmail.com>
2025-12-02 17:14:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,8 +8,18 @@ const nextConfig = {
     ignoreDuringBuilds: true
   },
   images: {
-    domains: ['eyhpjdnfeetmlayyxshx.supabase.co'],
-    unoptimized: true
+    domains: [
+      'eyhpjdnfeetmlayyxshx.supabase.co',
+      'mawater974.com',
+      'localhost',
+      '127.0.0.1'
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   },
   compiler: {
     removeConsole: false
