60e23aec4d06467bc586a6c6611a1a72f0a1f473
Mawater974 <mawater974@gmail.com>
2025-12-06 08:43:44 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,6 +20,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1
     },
     {
+      url: `${baseUrl}/cars`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9
+    },
+    {
       url: `${baseUrl}/login`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
