4861c396fdf13455e82662f1c7f36fb08a51ab62
Mawater974 <mawater974@gmail.com>
2025-12-07 09:15:35 +0300

    Update getCountryFromIP.ts

--- utils/getCountryFromIP.ts
+++ utils/getCountryFromIP.ts
@@ -8,10 +8,27 @@ export async function getCountryFromIP(): Promise<GeoInfo | null> {
     const response = await fetch('https://ipapi.co/json/');
     const data = await response.json();
 
+    // Log all data received from ipapi.co/json
+    console.log('Full IP API Response:', JSON.stringify(data, null, 2));
+
     if (data.error) {
       console.error('Error from IP API:', data.error);
       return null;
     }
+
+    // Log individual fields for easier inspection
+    console.log('IP:', data.ip);
+    console.log('City:', data.city);
+    console.log('Region:', data.region);
+    console.log('Country Code:', data.country_code);
+    console.log('Country Name:', data.country_name);
+    console.log('Continent Code:', data.continent_code);
+    console.log('Postal Code:', data.postal);
+    console.log('Latitude:', data.latitude);
+    console.log('Longitude:', data.longitude);
+    console.log('Timezone:', data.timezone);
+    console.log('Currency:', data.currency);
+    console.log('Languages:', data.languages);
 
     return {
       name: data.country_name,
