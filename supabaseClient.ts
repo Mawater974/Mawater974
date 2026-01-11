144b35b5fa9c69d19787fb9373be6916108abc9b
Mawater974 <mawater974@gmail.com>
2026-01-11 10:03:54 +0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL || 'https://ljbccbtufshhloprzity.supabase.co';
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqYmNjYnR1ZnNoaGxvcHJ6aXR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NDk3NzYsImV4cCI6MjA1NjUyNTc3Nn0.djps2FZEueEkiExQg-Yj86v2izYj6vb5Sn5oIByglTQ';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
